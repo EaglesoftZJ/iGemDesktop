2ad7e37f0296bc90b1fa981b01e50ed73b283907
n1ghtw1sh <116516842@qq.com>
2017-10-27 15:38:22 +0800

    bundle-id

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ mkdir build/out
 
 echo "Building All Platforms"
 
-electron-packager . 飞鸟 --platform=darwin --overwrite  --arch=all --app-bundle-id=cn.eaglesoft.飞鸟 --version=1.7.8 --out=build --icon=assets/app_icon --osx-sign.identity="Mac Developer: shanbo zhang (739HWZ3BL4)"
+electron-packager . 飞鸟 --platform=darwin --overwrite  --arch=all --app-bundle-id=cn.eaglesoft.flychat --version=1.7.8 --out=build --icon=assets/app_icon --osx-sign.identity="Mac Developer: shanbo zhang (739HWZ3BL4)"
 
 cd build/飞鸟-darwin-x64/
 
@@ -14,7 +14,7 @@ cd ../../
 
 # echo "Building Windows"
 
-electron-packager . 飞鸟 --platform=win32 --overwrite  --arch=all --app-bundle-id=cn.eaglesoft.飞鸟 --version=1.7.8 --out=build --icon=assets/app_icon.icns
+electron-packager . 飞鸟 --platform=win32 --overwrite  --arch=all --app-bundle-id=cn.eaglesoft.flychat --version=1.7.8 --out=build --icon=assets/app_icon.icns
 
 cd build
 zip --symlinks -r 飞鸟-Win-x86.zip 飞鸟-win32-ia32/
--- update.js
+++ update.js
@@ -1,4 +1,5 @@
 const { remote } = require('electron');
+let percent = 0;
 const updater = remote.require('electron-simple-updater');
 window.alert("update还是没载入");
 updater.on('update-available', (meta) => {
@@ -6,7 +7,11 @@ updater.on('update-available', (meta) => {
     window.alert(meta.readme);
     updater.downloadUpdate();
 });
-updater.on('update-downloading', () => { });
+updater.on('update-downloading', (value) => { 
+    if (percent === 0) {
+        window.alert(value);
+    }
+});
 updater.on('update-downloaded', () => {
     if (window.confirm('Restart and install updates?')) {
         updater.quitAndInstall();
